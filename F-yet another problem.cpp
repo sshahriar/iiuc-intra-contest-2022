72f64e97c69c24e0834e21df4766ee73678388ef
shahriar <30356865+sshahriar@users.noreply.github.com>
2022-08-21 07:17:03 +0600

    Update F-yet another problem.cpp

--- F-yet another problem.cpp
+++ F-yet another problem.cpp
@@ -1,63 +1,17 @@
 #include <bits/stdc++.h> 
 using namespace std; 
-#define ff              first
-#define ss              second
-#define pb(x)           push_back(x)
-#define mp              make_pair
-#define sz(x)           (int)x.size()
-#define mem(x ,y)       memset(x , y , sizeof x )
-#define all(a )         a.begin() , a.end()  
-#define pii             pair<int,int >
-#define endl            "\n"  
-#define forn(i,x,y )    for(int i= x; i<=y ; i++ )  
-#define ford(i,x,y )    for(int i= x; i>=y ; i-- )  
-#define rep(i,n )       for(int i= 0; i<n ; i++ ) 
-#define repi(i,n )      for(int i= 1; i<=n ; i++ ) 
-#define repit(i, c)     for( __typeof((c).begin()) i = (c).begin(); i != (c).end();++i )
-
-#ifdef  Local           
-    #define dbg(args...)    do {   cout << #args << ' ' ;  print(args); } while(0); cerr<< endl ;
-#else 
-    #define dbg(args...)   ; 
-#endif 
 
 typedef long long ll  ;
-typedef unsigned long long ull  ;
-ll  mod =  1e9+7, mod2 =     998244353  ;
 
- 
-// int setbit(int n, int pos  ) { return n = n|(1LL<< pos) ; }
-// int resetbit(int n,int pos ) { return n = n & ~(1LL<<pos ); }
-// bool checkbit(int n,int pos) { return (bool ) (n&(1LL<<pos))  ; }
-
-#include <ext/pb_ds/assoc_container.hpp> // Common file
-#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
-using namespace __gnu_pbds;
-template<class T> using ordered_set = tree<T, null_type , 
-    less_equal<T> , rb_tree_tag , tree_order_statistics_node_update> ;   
-template<typename T>ostream &operator<<(ostream & os, const ordered_set< T > &Set) {os << "[ "; repit(it , Set )os <<  *it  << ' ' ; return os << " ]\n" ; }  
-    // order_of_key (k) : Number of items strictly smaller than k .
-    // find_by_order(k) : K-th element in a set (counting from zero).
-    // less_equal for  multiset 
-   
-template<typename T> void print(const T& v) {    cout << v << ' ' ;}
-template<typename T1,typename... T2> void print( const T1& first, const T2&... rest ){ print(first); print(rest...) ;}
-template<typename T> void dbg_a(T a[] ,int n=10 ) {cerr << "[ "; for(int i=0;i <= n ; i++ )cerr<<a[i]<<' ' ; cerr<< " ]" <<  endl; } 
-template<typename F,typename S>ostream& operator<<( ostream& os, const pair< F, S > & p ){return os << "[ " << p.first << ", " << p.second << " ]";}
-template<typename T>ostream &operator<<(ostream & os, const vector< T > &v ){os << "[ "; for(int i=0; i<sz(v) ; i++ )  os << v[i] << ' ' ; return os << " ]\n" ; } 
-template<typename T>ostream &operator<<(ostream & os, const map< T ,T> &Map ){os << "[ "; repit(it , Map ) os << "[" <<(*it).ff << ' ' << (*it).ss << "] "    ; return os << "]\n" ; } 
-template<typename T>ostream &operator<<(ostream & os, const set< T > &Set  ){os << "[ "; repit(it , Set ) os <<  *it  << ' ' ; return os << " ]\n" ; }  
-template<typename T>ostream &operator<<(ostream & os, const multiset< T > &Set) {os << "[ "; repit(it , Set )os <<  *it  << ' ' ; return os << " ]\n" ; }  
- 
 #define  int ll 
 const int inf  =    1e9  ;   //0xc0  ;//0x3f ;   
 const int maxn   = (int)3e5 + 123 ;
-
-////////////////////////////////////////////////
-
+ 
 int n , m , k , flag =   0  ;  
 int a[maxn ]  , b[30]; 
 int l, r ;
+
+
 bool checkbit(int x ,int id ) {
     return   (bool)(x&(1<< id))  ;
 } 
@@ -76,12 +30,11 @@ int get_res() {
         for(int j=0 ; j<m ; j++ ) {
             if(checkbit(mask ,  j )  ) {
                 tot +=  b[ j] ; 
-                v.pb(b[j]) ;        
+                v.push_back(b[j]) ;        
             }
         } 
 
         if(tot % k  == 0  ) {
-            cout << v ;
             return 1 ;
         }
     }   
